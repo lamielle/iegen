6a7b0c4fece7199fb68ee4c1251f4f776ff7f781
Alan LaMielle <lamielle@cs.colostate.edu>
2008-09-05 20:22:49 +0000

    iegen: Added decorator library.  Added import tests for libraries.

---
+++ src/iegen/lib/test/_test.py
@@ -0,0 +1,57 @@
+from unittest import TestCase
+from nose.tools import raises
+
+#---------- Import Tests ----------
+#Test importing of iegen.lib modules
+class ImportTestCase(TestCase):
+
+	#Test simple importing of iegen.lib
+	def testImport(self):
+		try:
+			import iegen.lib
+		except Exception,e:
+			self.fail("'import iegen.lib' failed: "+str(e))
+
+	#Test simple importing of iegen.lib.ply
+	def testImportPLY(self):
+		try:
+			import iegen.lib.ply
+		except Exception,e:
+			self.fail("'import iegen.lib.ply' failed: "+str(e))
+
+	#Test simple importing of iegen.ast.visitor classes
+	def testNameImportPLY(self):
+		try:
+			from iegen.lib.ply import lex,yacc
+		except Exception,e:
+			self.fail("Importing classes from iegen.lib.ply failed: "+str(e))
+
+	#Test simple importing of iegen.lib.nose
+	def testImportNose(self):
+		try:
+			import iegen.lib.nose
+		except Exception,e:
+			self.fail("'import iegen.lib.nose' failed: "+str(e))
+
+	#Test simple importing of iegen.ast.visitor classes
+	def testNameImportNose(self):
+		try:
+			from iegen.lib.nose import DeprecatedTest,SkipTest,collector,main,run,run_exit,runmodule,with_setup
+
+		except Exception,e:
+			self.fail("Importing classes from iegen.lib.nose failed: "+str(e))
+
+	#Test simple importing of iegen.lib.decorator
+	def testImportDecorator(self):
+		try:
+			import iegen.lib.decorator
+		except Exception,e:
+			self.fail("'import iegen.lib.decorator' failed: "+str(e))
+
+	#Test simple importing of iegen.ast.visitor classes
+	def testNameImportDecorator(self):
+		try:
+			from iegen.lib.decorator import decorator,getinfo,new_wrapper
+		except Exception,e:
+			self.fail("Importing classes from iegen.lib.decorator failed: "+str(e))
+#----------------------------------
