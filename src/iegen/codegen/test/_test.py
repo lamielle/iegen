9227b71a052ef44e4b2d51fd301e1822934105cc
Alan LaMielle <lamielle@cs.colostate.edu>
2008-10-21 20:56:29 +0000

    iegen: Rearranged steps in moldyn-data-iter-reord.txt into calculation/codegen phases.

--- src/iegen/codegen/test/_test.py
+++ src/iegen/codegen/test/_test.py
@@ -17,7 +17,7 @@ class ImportTestCase(TestCase):
 			#_codegen.py
 			from iegen.codegen import calc_full_iter_space,get_bound_string,get_lower_bound_string,get_upper_bound_string
 			#_program_ast.py
-			from iegen.codegen import Program,Function,Statement
+			from iegen.codegen import Program,Function,Statement,VarDecl
 		except ImportError,e:
 			self.fail('Importing classes from iegen.codegen failed: '+str(e))
 #----------------------------------
