7c4912f5f0cd58e43454adad9140e207fa23d70d
Alan LaMielle <lamielle@cs.colostate.edu>
2008-11-05 17:00:11 +0000

    iegen/doc: Added presentation for the tiling meeting.

---
+++ doc/tiling_pres/sparse_comp.c
@@ -0,0 +1,8 @@
+   for(i=0; i<n_inter; i++){
+S1:  fx[inter1[i]]+=
+            x[inter1[i]]-
+            x[inter2[i]];
+S2:  fx[inter2[i]]+=
+            x[inter1[i]]-
+            x[inter2[i]];
+   }
