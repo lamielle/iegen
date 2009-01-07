539143d227c36756c7b7f132d88351ceba650a3f
Alan LaMielle <lamielle@cs.colostate.edu>
2009-01-07 18:08:30 +0000

    iegen/src/iegen/idg: Added stubs for four IDG node types.

--- src/iegen/idg/_idg.py
+++ src/iegen/idg/_idg.py
@@ -1,7 +1,29 @@
 #Inspector Dependence Graph
 
+#---------- IDGNode ----------
 class IDGNode(object):
 	__slots__=('deps','uses')
 
 	def __init__(self):
 		pass
+#-----------------------------
+
+#---------- IDGSymbolic ----------
+class IDGSymbolic(IDGNode):
+	pass
+#---------------------------------
+
+#---------- IDGDataSpace ----------
+class IDGDataSpace(IDGNode):
+	pass
+#----------------------------------
+
+#---------- IDGRelation ----------
+class IDGRelation(IDGNode):
+	pass
+#---------------------------------
+
+#---------- IDGCall ----------
+class IDGCall(IDGNode):
+	pass
+#-----------------------------
