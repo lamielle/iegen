1569e8a2360fd0328ffea4ab55df9cf808741b05
Alan LaMielle <lamielle@cs.colostate.edu>
2009-03-23 07:31:27 -0600

    Updates to IEGenObject, settings and printing
    The settings field of IEGenObject is now a class member (static) rather
    than an instance member.  This is necessary as a new IEGenSettings
    object cannot be created for each instance of IEGenObject.
    
    Added two versions of info_print: instance and static versions.  This
    method should be used rather than just using the print statement when
    information needs to be printed.
    
    Added a static version of debug_print.
    
    References #27.

--- src/iegen/_iegen_object.py
+++ src/iegen/_iegen_object.py
@@ -3,12 +3,29 @@ class IEGenObject(object):
 	__slots__=('settings',)
 
 	class IEGenSettings(object): pass
+	settings=IEGenSettings()
 
-	def __init__(self):
-		self.settings=IEGenObject.IEGenSettings()
+	def __init__(self): pass
 
 	#Helper method for debug printing
 	#Will print only if self.settings.debug is True
 	def debug_print(self,output):
-		if self.settings.aether.debug: print output
+		if self.settings.debug: print output
+
+	#Static helper method for debug printing
+	#Will print only if IEGenObject.settings.debug is True
+	def debug_print(output):
+		if IEGenObject.settings.debug: print output
+	debug_print=staticmethod(debug_print)
+
+	#Helper method for printing information messages
+	#Will print only if self.settings.quiet is False
+	def info_print(self,output):
+		if not self.settings.quiet: print output
+
+	#Static helper method for printing information messages
+	#Will print only if IEGenObject.settings.quiet is False
+	def info_print(output):
+		if not IEGenObject.settings.quiet: print output
+	info_print=staticmethod(info_print)
 #---------------------------------------
