15240165d25507727192b3e5ac00e7448ad34cdc
Alan LaMielle <lamielle@cs.colostate.edu>
2009-05-31 12:36:41 -0600

    Updated pycloog tests
    References #133: The pycloog tests were updated and a new test was added
    to make sure that scattering functions are working as expected.

--- src/iegen/pycloog/test/_test.py
+++ src/iegen/pycloog/test/_test.py
@@ -22,64 +22,6 @@ class ImportTestCase(TestCase):
 #---------- PYCLooG Tests ----------
 class PYCLooGTestCase(TestCase):
 
-	def testPYCLooGSimple(self):
-		from iegen.pycloog import Statement,codegen
-		from iegen import Set,Symbolic
-
-		code='''if ((m >= 1) && (n >= 1)) {
-  for (i=1;i<=n;i++) {
-    for (j=1;j<=m;j++) {
-      S1(i,j);
-    }
-  }
-  for (i=1;i<=n;i++) {
-    for (j=1;j<=m;j++) {
-      S0(i,j);
-    }
-  }
-}'''
-
-		old_code='''if ((m >= 1) && (n >= 1)) {
-  for (i=1;i<=n;i++) {
-    for (j=1;j<=m;j++) {
-      S1 ;
-    }
-  }
-  for (i=1;i<=n;i++) {
-    for (j=1;j<=m;j++) {
-      S0 ;
-    }
-  }
-}'''
-
-		old_old_code='''if (m >= 1) {
-  for (i=1;i<=n;i++) {
-    for (j=1;j<=m;j++) {
-      S2 ;
-    }
-  }
-}
-if (m >= 1) {
-  for (i=1;i<=n;i++) {
-    for (j=1;j<=m;j++) {
-      S1 ;
-    }
-  }
-}'''
-
-		dom1=Set('{[i,j]:1<=i and i<=n and 1<=j and j<=m and m>=1}',[Symbolic('n'),Symbolic('m')])
-		scat1=Set('{[p1,p2,i,j,n,m]: p1=1 and p2=1}')
-
-		dom2=Set('{[i,j]:1<=i and i<=n and 1<=j and j<=m and m>=1}',[Symbolic('n'),Symbolic('m')])
-		scat2=Set('{[p1,p2,i,j,n,m]: p1=0 and p2=0}')
-
-		stmt1=Statement(dom1,scat1)
-		stmt2=Statement(dom2,scat2)
-		stmts=(stmt1,stmt2)
-
-		res=codegen(stmts)
-		self.failUnless(code==res,'PYCLooG generated:\n\n%s\n\nbut test expected:\n\n%s'%(res,code))
-
 	def testPYCLooGNoScattering(self):
 		from iegen.pycloog import Statement,codegen
 		from iegen import Set,Symbolic
@@ -115,6 +57,93 @@ if (m >= 1) {
 		stmt1=Statement(dom1)
 		stmt2=Statement(dom2)
 		stmts=(stmt1,stmt2)
+
+		res=codegen(stmts)
+		self.failUnless(code==res,'PYCLooG generated:\n\n%s\n\nbut test expected:\n\n%s'%(res,code))
+
+	def testPYCLooGSimple(self):
+		from iegen.pycloog import Statement,codegen
+		from iegen import Set,Relation,Symbolic
+
+		code='''for (i=0;i<=n;i++) {
+  for (j=0;j<=m;j++) {
+    S1(i,j);
+  }
+}
+for (i=0;i<=n;i++) {
+  for (j=0;j<=m;j++) {
+    S0(i,j);
+  }
+}'''
+
+		old_code='''if ((m >= 1) && (n >= 1)) {
+  for (i=1;i<=n;i++) {
+    for (j=1;j<=m;j++) {
+      S1 ;
+    }
+  }
+  for (i=1;i<=n;i++) {
+    for (j=1;j<=m;j++) {
+      S0 ;
+    }
+  }
+}'''
+
+		old_old_code='''if (m >= 1) {
+  for (i=1;i<=n;i++) {
+    for (j=1;j<=m;j++) {
+      S2 ;
+    }
+  }
+}
+if (m >= 1) {
+  for (i=1;i<=n;i++) {
+    for (j=1;j<=m;j++) {
+      S1 ;
+    }
+  }
+}'''
+
+		dom1=Set('{[i,j]:0<=i and i<=n and 0<=j and j<=m}',[Symbolic('n'),Symbolic('m')])
+		scat1=Relation('{[i,j]->[p0,i,p1,j,p2]: p0=1 and p1=0 and p2=0}')
+
+		dom2=Set('{[i,j]:0<=i and i<=n and 0<=j and j<=m}',[Symbolic('n'),Symbolic('m')])
+		scat2=Relation('{[i,j]->[p0,i,p1,j,p2]: p0=0 and p1=0 and p2=0}')
+
+		stmt1=Statement(dom1,scat1)
+		stmt2=Statement(dom2,scat2)
+		stmts=(stmt1,stmt2)
+
+		res=codegen(stmts)
+		self.failUnless(code==res,'PYCLooG generated:\n\n%s\n\nbut test expected:\n\n%s'%(res,code))
+
+	def testPYCLooGScatter(self):
+		from iegen.pycloog import Statement,codegen
+		from iegen import Set,Relation,Symbolic
+
+		code='''for (s=0;s<=T;s++) {
+  for (i=0;i<=N;i++) {
+    S0(s,i);
+  }
+  for (i=0;i<=n_inter;i++) {
+    S1(s,i);
+    S2(s,i);
+  }
+}'''
+
+		dom0=Set('{[s,i]: 0<=s && s<=T && 0<=i && i<=N}',[Symbolic('T'),Symbolic('N')])
+		scat0=Relation('{[s,i]->[c0,s,c1,i,c2]: c0=0 && c1=0 && c2=0}')
+
+		dom1=Set('{[s,i]: 0<=s && s<=T && 0<=i && i<=n_inter}',[Symbolic('T'),Symbolic('n_inter')])
+		scat1=Relation('{[s,i]->[c0,s,c1,i,c2]: c0=0 && c1=1 && c2=0}')
+
+		dom2=Set('{[s,i]: 0<=s && s<=T && 0<=i && i<=n_inter}',[Symbolic('T'),Symbolic('n_inter')])
+		scat2=Relation('{[s,i]->[c0,s,c1,i,c2]: c0=0 && c1=1 && c2=1}')
+
+		stmt0=Statement(dom0,scat0)
+		stmt1=Statement(dom1,scat1)
+		stmt2=Statement(dom2,scat2)
+		stmts=(stmt0,stmt1,stmt2)
 
 		res=codegen(stmts)
 		self.failUnless(code==res,'PYCLooG generated:\n\n%s\n\nbut test expected:\n\n%s'%(res,code))
