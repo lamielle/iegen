f6109569de0ab65af3d1ce1e7c3abc2d5210516d
Alan LaMielle <lamielle@cs.colostate.edu>
2009-05-31 11:16:30 -0600

    Updated TransVisitor to translate relations
    References #133: The TransVisitor now translates a given Set into a
    union of CLooG matrix domains and translates a given Relation with a
    single conjunction into a scattering function in CLooG matrix format.
    Tests were updated and added to ensure this visitor is still working
    properly for Sets and that it works properly for Relations.

--- src/iegen/ast/visitor/_trans_visitor.py
+++ src/iegen/ast/visitor/_trans_visitor.py
@@ -1,53 +1,121 @@
 from iegen.ast.visitor import DFVisitor
 
-#Translation Visitor:
-#Only works on a Set, not a Relation
-#Given a Set, produces a collection of domain matrices,
-#one for each PresSet in the Set.
-#The result is placed in the 'mats' attribute
+#Translation Visitor: Translate the visited Set or Relation into
+# a matrix representing a domain or scattering function in
+# CLooG format.
+#
+#Briefly, this format is a matrix that represents a polyhedron.
+#Each row of the matrix is one constraint, either equality or inequality.
+#
+#For domain constraint matrices, the columns are ordered as follows:
+#eq/in iterators parameters constant
+#Col 1: {0,1} 0=equality 1=inequality
+#Col 2-num iterators+1: coefficients on the iterators in this constraint
+#Col num iterators+2-num iterators+num parameters+1: coefficients on the
+# parameters (symbolics)
+#Col num iterators+num parameters+2: constraint constant
+#
+#For scattering function constraint matrices, the columns are ordered as follows:
+#eq/in out-vars in-vars parameters constant
+#Col 1: {0,1} 0=equality 1=inequality
+#Col 2-num out-vars+1: identity row (1 for associated out var, 0s elsewhere)
+#Col num out-vars+2-num out-vars num in-vars+1: input var coefficients
+#Col num out-vars+num in-vars+2-num out-vars+num in-vars+num params+1: parameter coefficients
+#Col num out-vars+num in-vars+num params+2: constraint constant
+#
+#Multiple conjunctions of PresSet objects in the Set (for a domain)
+# will be translated to a collection of constraint matrices
+# (a union of polyhedra).
+#Multiple conjunctions of PresRelation objects in the Relation
+# are not supported, a ValueError will be raised if this is detected.
+#
+#This visitor requires as input a collection of names of parameters.
+#This is needed so that multiple uses of this visitor have the same parameter
+# columns in common.
+#
+#The result of this visitor is placed in the mats attribute.
+#In the case of a domain (Set) this may have multiple matrices.
+#In the case of a scattering function (Relation) this will have a single matrix.
 class TransVisitor(DFVisitor):
-	def __init__(self):
-		self.params=None
-		self.num_params=0
+	def __init__(self,params):
+		self.params=params
+
 		self.at_var_tuple=False
 
+		#---------- Visiting state variables ----------
+		#Are we within a Set?
+		self.in_set=False
+
+		#Are we within a Relation?
+		self.in_relation=False
+
+		#Dictionary of name -> column position mappings
+		self.name_dict=None
+		#----------------------------------------------
+
+	def calc_name_dict(self,var_names):
+		names=var_names+self.params
+		name_dict={}
+
+		for pos,name in enumerate(names):
+			name_dict[name]=pos+1
+
+		return name_dict
+
+	#Calculates the number of columns in the matrix we are creating
+	def calc_num_cols(self):
+		#1 column for the eq/in column
+		#len(self.name_dict) columns
+		#1 column for the constant column
+		return 1+len(self.name_dict)+1
+
 	def inSet(self,node):
+		#Starting to visit a Set, init the result list
 		self.mats=[]
 
 	def inPresSet(self,node):
+		#Starting to translate a PresSet to a matrix, init the result matrix
 		self._mat=[]
 
-		#Get the params for this PresSet
-		self.params=[sym.name for sym in node.symbolics]
-		self.num_params=len(self.params)
+		#Build mappings for tuple variables and symbolics to column index
+		self.name_dict=self.calc_name_dict([var.id for var in node.tuple_set.vars])
 
 	def outPresSet(self,node):
+		#Append the current result matrix to the result matrix collection
 		self.mats.append(self._mat)
 
 	def inRelation(self,node):
-		raise ValueError('This visitor only works on Sets.')
+		#Make sure this Relation has only a single conjunction
+		if len(node.relations)!=1:
+			raise ValueError('Translation of multiple Relation conjunctions is not supported')
 
 	def inPresRelation(self,node):
-		raise ValueError('This visitor only works on Sets.')
+		#Starting to translate a PresRelation to a matrix, init the result matrix
+		self._mat=[]
+
+		#Build mappings for tuple variables and symbolics to column index
+		self.name_dict=self.calc_name_dict([var.id for var in node.tuple_out.vars+node.tuple_in.vars])
+
+	def outPresRelation(self,node):
+		self.mat=self._mat
 
 	def inVarTuple(self,node):
-		self.vars=[var.id for var in node.vars]
-		self.num_vars=len(self.vars)
-		self.num_cols=1+self.num_vars+self.num_params+1
 		self.at_var_tuple=True
 
 	def outVarTuple(self,node):
 		self.at_var_tuple=False
 
 	def inInequality(self,node):
-		self._row=[0]*self.num_cols
+		#Create a new row for an inequality constraint
+		self._row=[0]*self.calc_num_cols()
 		self._row[0]=1
 
 	def outInequality(self,node):
 		self._mat.append(self._row)
 
 	def inEquality(self,node):
-		self._row=[0]*self.num_cols
+		#Create a new row for an equality constraint
+		self._row=[0]*self.calc_num_cols()
 		self._row[0]=0
 
 	def outEquality(self,node):
@@ -55,18 +123,16 @@ class TransVisitor(DFVisitor):
 
 	def inVarExp(self,node):
 		if not self.at_var_tuple:
-			#Calculate the position of this variable in the matrix
-			if node.id in self.vars:
-				pos=self.vars.index(node.id)+1
-			elif node.id in self.params:
-				pos=self.params.index(node.id)+self.num_vars+1
-			else:
-				raise ValueError('Existential variable in set.')
+			#Get the column of this variable in the matrix
+			try:
+				pos=self.name_dict[node.id]
 
-			#Assign this variable's coefficient to the matrix at the clculated position
-			self._row[pos]=node.coeff
+				#Assign this variable's coefficient to the matrix in the proper column
+				self._row[pos]=node.coeff
+			except KeyError,e:
+				raise ValueError("Variable '%s' is either a free variable or was not specified as a parameter"%(node.id))
 
-	#Cannot translate sets with functions
+	#Cannot translate formulas with functions
 	def inFuncExp(self,node):
 		raise ValueError('Translation of function expressions is not supported.')
 
