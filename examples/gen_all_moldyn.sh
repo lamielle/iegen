c1e88e6a41d44cc91314fe60567cf5d67319a5bf
Alan LaMielle <lamielle@cs.colostate.edu>
2010-08-30 09:04:26 -0600

    Moves gen_all_* scripts to examples/

---
+++ examples/gen_all_moldyn.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env sh
+
+cd ..
+./waf install
+
+IEGEN=./iegen/bin/iegen
+
+OUT_DIR=$HOME/RTRTcode/trunk/src/drivers/moldyn
+
+#-----no_trans-----
+VERSION=notrans_gen
+echo
+echo "-----Generating version ${VERSION}-----"
+echo
+$IEGEN examples/moldyn_full.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+
+#-----cpack_only-----
+VERSION=cpack_only_gen
+echo
+echo "-----Generating version ${VERSION}-----"
+echo
+$IEGEN examples/moldyn_full_cpack_only.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+
+#-----cpack_align-----
+VERSION=cpack_align_gen
+echo
+echo "-----Generating version ${VERSION}-----"
+echo
+$IEGEN examples/moldyn_full_cpack_align.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+
+#-----cpack_align_ptrupdate-----
+VERSION=cpack_align_ptrupdate_gen
+echo
+echo "-----Generating version ${VERSION}-----"
+echo
+$IEGEN examples/moldyn_full_cpack_align_ptrupdate.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+
+#-----cpack_align_ptrupdate_lexmin-----
+VERSION=cpack_align_ptrupdate_lexmin_gen
+echo
+echo "-----Generating version ${VERSION}-----"
+echo
+$IEGEN examples/moldyn_full_cpack_align_ptrupdate_lexmin.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+
+#-----cpack_align_ptrupdate_lexmin_ptrupdate-----
+VERSION=cpack_align_ptrupdate_lexmin_ptrupdate_gen
+echo
+echo "-----Generating version ${VERSION}-----"
+echo
+$IEGEN examples/moldyn_full_cpack_align_ptrupdate_lexmin_ptrupdate.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+
+#-----cpack_align_ptrupdate_lexmin_ptrupdate_fst-----
+VERSION=cpack_align_ptrupdate_lexmin_ptrupdate_fst_gen
+echo
+echo "-----Generating version ${VERSION}-----"
+echo
+$IEGEN examples/moldyn_full_cpack_align_ptrupdate_lexmin_ptrupdate_fst.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+
+#-----cpack_align_ptrupdate_lexmin_ptrupdate_fst_spo-----
+VERSION=cpack_align_ptrupdate_lexmin_ptrupdate_fst_spo_gen
+echo
+echo "-----Generating version ${VERSION}-----"
+echo
+$IEGEN examples/moldyn_full_cpack_align_ptrupdate_lexmin_ptrupdate_fst_sparseloop.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
