d1cab1a77264a87d03718c4d0b12d3da3c224db5
Alan LaMielle <lamielle@cs.colostate.edu>
2010-08-30 09:03:24 -0600

    Adds gen_all_spmv.sh script

---
+++ gen_all_spmv.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+./waf install
+
+IEGEN=./iegen/bin/iegen
+
+OUT_DIR=$HOME/RTRTcode/trunk/src/drivers/spmv
+
+VERSIONS="
+notrans
+"
+
+#Generate each version
+for VERSION in $VERSIONS
+do
+  echo
+  echo "-----Generating version ${VERSION}-----"
+  echo
+  $IEGEN examples/spmv_$VERSION.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/spmv_${VERSION}.c
+done
