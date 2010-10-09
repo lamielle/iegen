815b6b5761f8e2469ecbbbbb2873f3d0651c059b
Alan LaMielle <lamielle@cs.colostate.edu>
2010-10-09 11:30:15 -0600

    Updates gen_all_* scripts to stop on errors
    Updates the gen_all_moldyn.sh and gen_all_spmv.sh scripts to stop generating
    code if one of the versions fails.

--- examples/gen_all_moldyn.sh
+++ examples/gen_all_moldyn.sh
@@ -6,58 +6,90 @@ IEGEN=./iegen/bin/iegen
 
 OUT_DIR=$HOME/RTRTcode/trunk/src/drivers/moldyn
 
+exit_on_error() {
+#Stop running if the program didn't exit with a 0 return value↲
+if [ $1 != 0 ]; then
+  echo "---------- Command exited with a non-zero exit code ($1), quitting... ----------"↲
+  exit 1
+fi
+}
+
 #-----no_trans-----
 VERSION=notrans_gen
 echo
 echo "-----Generating version ${VERSION}-----"
 echo
-$IEGEN examples/moldyn_full.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+command="$IEGEN examples/moldyn_full.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c"
+echo $command
+$command
+exit_on_error $?
 
 #-----cpack_only-----
 VERSION=cpack_only_gen
 echo
 echo "-----Generating version ${VERSION}-----"
 echo
-$IEGEN examples/moldyn_full_cpack_only.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+command="$IEGEN examples/moldyn_full_cpack_only.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c"
+echo $command
+$command
+exit_on_error $?
 
 #-----cpack_align-----
 VERSION=cpack_align_gen
 echo
 echo "-----Generating version ${VERSION}-----"
 echo
-$IEGEN examples/moldyn_full_cpack_align.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+command="$IEGEN examples/moldyn_full_cpack_align.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c"
+echo $command
+$command
+exit_on_error $?
 
 #-----cpack_align_ptrupdate-----
 VERSION=cpack_align_ptrupdate_gen
 echo
 echo "-----Generating version ${VERSION}-----"
 echo
-$IEGEN examples/moldyn_full_cpack_align_ptrupdate.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+command="$IEGEN examples/moldyn_full_cpack_align_ptrupdate.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c"
+echo $command
+$command
+exit_on_error $?
 
 #-----cpack_align_ptrupdate_lexmin-----
 VERSION=cpack_align_ptrupdate_lexmin_gen
 echo
 echo "-----Generating version ${VERSION}-----"
 echo
-$IEGEN examples/moldyn_full_cpack_align_ptrupdate_lexmin.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+command="$IEGEN examples/moldyn_full_cpack_align_ptrupdate_lexmin.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c"
+echo $command
+$command
+exit_on_error $?
 
 #-----cpack_align_ptrupdate_lexmin_ptrupdate-----
 VERSION=cpack_align_ptrupdate_lexmin_ptrupdate_gen
 echo
 echo "-----Generating version ${VERSION}-----"
 echo
-$IEGEN examples/moldyn_full_cpack_align_ptrupdate_lexmin_ptrupdate.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+command="$IEGEN examples/moldyn_full_cpack_align_ptrupdate_lexmin_ptrupdate.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c"
+echo $command
+$command
+exit_on_error $?
 
 #-----cpack_align_ptrupdate_lexmin_ptrupdate_fst-----
 VERSION=cpack_align_ptrupdate_lexmin_ptrupdate_fst_gen
 echo
 echo "-----Generating version ${VERSION}-----"
 echo
-$IEGEN examples/moldyn_full_cpack_align_ptrupdate_lexmin_ptrupdate_fst.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+command="$IEGEN examples/moldyn_full_cpack_align_ptrupdate_lexmin_ptrupdate_fst.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c"
+echo $command
+$command
+exit_on_error $?
 
 #-----cpack_align_ptrupdate_lexmin_ptrupdate_fst_spo-----
 VERSION=cpack_align_ptrupdate_lexmin_ptrupdate_fst_spo_gen
 echo
 echo "-----Generating version ${VERSION}-----"
 echo
-$IEGEN examples/moldyn_full_cpack_align_ptrupdate_lexmin_ptrupdate_fst_sparseloop.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c
+command="$IEGEN examples/moldyn_full_cpack_align_ptrupdate_lexmin_ptrupdate_fst_sparseloop.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/moldyn_full_${VERSION}.c"
+echo $command
+$command
+exit_on_error $?
--- examples/gen_all_spmv.sh
+++ examples/gen_all_spmv.sh
@@ -8,7 +8,16 @@ OUT_DIR=$HOME/RTRTcode/trunk/src/drivers/spmv
 
 VERSIONS="
 notrans
+cpack
 "
+
+exit_on_error() {
+#Stop running if the program didn't exit with a 0 return value↲
+if [ $1 != 0 ]; then
+  echo "---------- Command exited with a non-zero exit code ($1), quitting... ----------"↲
+  exit 1
+fi
+}
 
 #Generate each version
 for VERSION in $VERSIONS
@@ -16,5 +25,8 @@ do
   echo
   echo "-----Generating version ${VERSION}-----"
   echo
-  $IEGEN examples/spmv_$VERSION.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/spmv_${VERSION}.c
+  command="$IEGEN examples/spmv_$VERSION.spec --inspector-name=inspector_${VERSION} --executor-name=executor_${VERSION} -o $OUT_DIR/spmv_${VERSION}.c"
+  echo $command
+  $command
+  exit_on_error $?
 done
